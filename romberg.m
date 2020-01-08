4afe3df48afff81873863132cc3682a43039fc02
MKKOW <56612182+MKKOW@users.noreply.github.com>
2020-01-08 11:09:35 +0100

    Romberg1.0

---
+++ romberg.m
@@ -0,0 +1,18 @@
+function [x,R]=romberg(f,a,b,M)
+%Funkcja oblicza wartosc numerczna calki na przedziale (a,b) z funkcji f(x)
+h=b-a;
+R=zeros(M+1);
+R(1,1)=1/2*(b-a)*(f(a)+f(b));
+for n = 2:(M+1)
+    h=h/2;
+    suma=0;
+    for i=1:(2^(n-2))
+        suma=suma+f(a+(2*i-1)*h);
+    end
+    R(n,1)=1/2*R(n-1,1)+h*suma;
+    for m=2:n
+        R(n,m)=R(n,m-1)+(R(n,m-1)-R(n-1,m-1))/(4^(m-1)-1);
+    end
+end
+x=R(M,M);
+end
